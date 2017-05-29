379ec72dc1c7d7cb35f25f2fd5835215d8e03aaa
BuildTools <louis.grenzebach@gmail.com>
2017-05-28 17:28:57 -0700

    Added tarot, cleaned up the logic;

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
 import json
-import random
-import string
-import re
-import sys
-import discord
 import logging
-import time
+import random
+import re
+import string
+import sys
 
+import discord
+from card_picker.Deck import Deck
 from dice_roller.DiceThrower import DiceThrower
 
+# https://regex101.com/r/SrVpEg/2
 
-dice_expression = re.compile(r"(?:\[)([\w_:+-,<>=()]+)(?:\])?")
-coin_expression = re.compile(r"(?:\[)(coin)")
-room_expression = re.compile(r"(?:\[)(room)")
 
 dice = DiceThrower()
 root = logging.getLogger('bot')
 client = discord.Client()
 
-def main():
 
+def main():
     # load config
     with open('config.json', 'r') as f:
         config = json.load(f)
@@ -48,6 +46,8 @@ async def on_message(message):
         return
 
     await client.change_presence(game=discord.Game(name='RNG the Game'))
+
+    # remove bot from message if included (in @botname scenario)
     if client.user in message.mentions:
         directed = True
         root.info('Directed Message Received')
@@ -57,75 +57,95 @@ async def on_message(message):
         directed = False
         plain_message = message.content
 
+    # simplify the message format
     plain_message = plain_message.lower().strip(string.whitespace)
 
-    dice_message = dice_expression.match(plain_message)
-    coin_message = coin_expression.match(plain_message)
-    room_message = room_expression.match(plain_message)
+    # expressions
+    command_expression = re.compile(r"(?:!)(([0-9]+)(d|c|r|t)([\w_:+-,<>=()]*))(?:\|)?([\w{}\[\] ]*)")
+    template_expression = re.compile(r"(?:!)([A-Za-z]+)([0-9+,]*)")
 
-    if coin_message:
-        await client.send_typing(message.channel)
-        msg = random.choice(['heads', 'tails'])
-        await client.send_message(message.channel, msg)
+    # check if a template needs applied
+    template_message = template_expression.match(plain_message)
+    if template_message:
+        reformed_message = apply_template(template_message.group(1), template_message.group(2))
+        if reformed_message:
+            plain_message = reformed_message
+            reformed_message = True
 
-    elif room_message:
+    # convert the message to our command format
+    command_message = command_expression.match(plain_message)
+
+    # execute the request
+    if command_message:
+        root.info("parsed command "
+              + "Count:" + command_message.group(2)
+              + " Role:" + command_message.group(3)
+              + " Args:" + command_message.group(4)
+              + " Template:" + command_message.group(5))
+
         await client.send_typing(message.channel)
+        result = get_message(command_message.group(1), int(command_message.group(2)), command_message.group(3),
+                          command_message.group(4))
+
+    else:
+        print("command not parsed " + plain_message)
+        return
+
+    # apply a template to the result (if requested)
+    result_template = command_message.group(5)
+    if (len(result_template) > 0):
+        msg = result_template.format(s=result)
+    else:
+        msg = result
+
+    await client.send_message(message.channel, msg)
+
+def get_message(full_command, count, role, args):
+    if role == 't':
+        if len(args) > 0:
+            deck = Deck(args)
+        else:
+            deck = Deck('shadow')
+        deck.create()
+        deck.shuffle()
+        hand = deck.deal(count)
+        return hand
+
+    elif role == 'c':
+        x = 1
+        bag = []
+        while x <= int(count):
+            bag.append(random.choice(['heads', 'tails']))
+            x += 1
+        return bag
+
+    elif role == 'r':
         members = client.get_all_members()
         actives = []
         for member in members:
             if str(member.status) == "online":
                 actives.append(member)
-        member = random.choice(actives)
+        x = 1
+        bag = []
+        random.shuffle(actives)
+        while x <= int(count) and len(actives) > 0:
+            bag.append(actives.pop().display_name)
+            x += 1
+        return bag
 
-        msg = "I choose " + str(member.display_name)
+    elif role == 'd':
+        msg = dice.throw(full_command)
+        return msg
 
-        await client.send_message(message.channel, msg)
-
-    elif dice_message:
-        await client.send_typing(message.channel)
-        trim_msg = dice_message.group(1)
-        msg = get_dice(trim_msg)
-        if not msg:
-            msg = 'Unable to parse input'
-        await client.send_message(message.channel, msg)
-    return
-
-
-def get_dice(command):
-    root.info('Received Command %s', command)
-
-    # apply template
-    if command[0].isalpha():
-        root.info('Template Format %s Received', command)
-        template = re.match(r"([A-Za-z]+)([0-9+,]*)", command)
-        if template:
-            command = apply_template(template.group(1), template.group(2))
-
-    if command is False:
-        return None
-
-    # get output format
-    result_template = str()
-    if "|" in command:
-        command, result_template = command.split("|", 1)
-
-    root.info('Formatted Command %s',command)
-    score = dice.throw(command)
-
-    if (len(result_template) > 0):
-        msg = result_template.format(s=score)
-    else:
-        msg = score
-
-    return msg
+    return ''
 
 
 def apply_template(template, value=''):
     root.info('Template Parsed to %s %s', template, value)
     return {
-        'sr': value + 'd6>=5f=1|{s[modified]} {s[success]} successes {s[fail]} fail',
-        'f': '4d3-2' + value + '|{s[total]}',
-        'w': '2d6+0' + value + '|{s[total]}'
+        'sr': '!' + value + 'd6>=5f=1|{s[modified]} {s[success]} successes {s[fail]} fail',
+        'f': '!' + '4d3-2' + value + '|{s[total]}',
+        'w': '!' + '2d6+0' + value + '|{s[total]}'
     }.get(template, False)
 
 
